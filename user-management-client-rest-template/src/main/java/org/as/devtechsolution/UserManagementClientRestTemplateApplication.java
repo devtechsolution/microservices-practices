32f8c5d109591c69bca136acaaa7845d01b7f30c
adityasrivastva <aditya5srivastva@gmail.com>
2020-11-04 18:32:48 +0530

    Initial Commit user-management-client-rest-template

---
+++ user-management-client-rest-template/src/main/java/org/as/devtechsolution/UserManagementClientRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.as.devtechsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementClientRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementClientRestTemplateApplication.class, args);
+	}
+
+}
