9af5886040294ddf95372f970a9dbf6a99facd74
adityasrivastva <aditya5srivastva@gmail.com>
2020-11-09 08:34:37 +0530

    Implemented filters for zuul api gateway

--- api-gateway/ehospital-zuul-api-gateway/src/main/java/org/as/devtechsolution/EhospitalZuulApiGatewayApplication.java
+++ api-gateway/ehospital-zuul-api-gateway/src/main/java/org/as/devtechsolution/EhospitalZuulApiGatewayApplication.java
@@ -2,8 +2,10 @@ package org.as.devtechsolution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class EhospitalZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
